fe7d39863ba7610e34e4fe79a9a3ab48afb85503
William <williamjrobinson@me.com>
2020-12-06 16:57:11 +0000

    Added Instagram stories

--- SharingSpike/ViewController.swift
+++ SharingSpike/ViewController.swift
@@ -7,10 +7,14 @@
 
 import UIKit
 import LinkPresentation
+import MobileCoreServices
 
 class ViewController: UIViewController {
 
+    // IRL url & the promo image will be on the episode object
     let url = URL(string: "https://www.bbc.co.uk/iplayer/episode/m0008s9l")
+    let promoImage = UIImage(named: "promoImage")
+
     var metadata: LPLinkMetadata?
 
     @IBOutlet weak var button: UIButton!
@@ -42,10 +46,12 @@ class ViewController: UIViewController {
         }
     }
 
-
     @IBAction func buttonPressed(_ sender: UIButton) {
 
-        let activityViewController = UIActivityViewController(activityItems: [self],
+        // Without an image provided Instagram doesn't show up
+        let itemProvider = UIActivityItemProvider(placeholderItem: promoImage!)
+
+        let activityViewController = UIActivityViewController(activityItems: [self, itemProvider],
                                                               applicationActivities: nil)
 
         activityViewController.popoverPresentationController?.sourceView = sender
@@ -55,7 +61,6 @@ class ViewController: UIViewController {
                 animated: true,
                 completion: nil)
     }
-
 }
 
 extension ViewController: UIActivityItemSource {
@@ -64,6 +69,30 @@ extension ViewController: UIActivityItemSource {
     }
 
     func activityViewController(_ activityViewController: UIActivityViewController, itemForActivityType activityType: UIActivity.ActivityType?) -> Any? {
+
+        guard let activityType = activityType else {
+            print("😎 no idea")
+            return nil
+        }
+
+        if activityType.rawValue.contains("instagram") {
+
+            guard let url = URL(string: "instagram-stories://share"),
+                  let promoImage = promoImage,
+                  let imageData = promoImage.pngData() else {
+                return nil
+            }
+
+            let items = [["com.instagram.sharedSticker.backgroundTopColor": "#EA2F3F",
+                          "com.instagram.sharedSticker.backgroundBottomColor": "#8845B9",
+                          "com.instagram.sharedSticker.stickerImage" : imageData]]
+
+            let pasteboardOptions = [UIPasteboard.OptionsKey.expirationDate: Date().addingTimeInterval(60*5)]
+            UIPasteboard.general.setItems(items, options: pasteboardOptions)
+
+            return UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
+
         return metadata?.originalURL
     }
 
